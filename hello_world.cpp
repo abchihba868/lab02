f6376fe57ae83b09151875e838c68eb0ec0014f3
abchihba868 <ps.obukhov@gmail.com>
2025-05-31 17:35:05 +0300

    Улучшен код программы

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,9 @@
 int
 main()
 {
-  std::cout << "Hello world from " << std::endl;
+std::cout << "Введите имя пользователя: ";
+  std::string name;
+  std::getline(std::cin, name);
+  std::cout << "Hello world from " << name << std::endl;
   return 0;
 }
